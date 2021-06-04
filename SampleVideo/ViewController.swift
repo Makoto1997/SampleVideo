5946c9e1ae8585563d3c89ec9bbf013f85d62b9b
Makoto <>
2021-06-04 23:24:43 +0900

    Initial Commit

---
+++ SampleVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleVideo
+//
+//  Created by Makoto on 2021/06/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
